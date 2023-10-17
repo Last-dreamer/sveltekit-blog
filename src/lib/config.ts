4650d1043b865c8d5400732966dbf56266e1f21b
Last-dreamer <asim0000.ak@gmail.com>
2023-10-17 22:31:39 +0400

    adding layout

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = 'Lost Dreamer'
+export const description = "Lost Dreamer Markdown blog"
+export const url =  dev ? "http://127.0.0.1:5173/" : "url"
